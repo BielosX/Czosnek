8645a27c3c4c37aecd43fbeedaa1b0e53b8d951d
Mateusz Biel <matbiel@gmail.com>
2023-09-02 21:24:02 +0200

    Apply spotless plugin

--- src/main/java/com/czosnek/CzosnekApplication.java
+++ src/main/java/com/czosnek/CzosnekApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CzosnekApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CzosnekApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(CzosnekApplication.class, args);
+  }
 }
