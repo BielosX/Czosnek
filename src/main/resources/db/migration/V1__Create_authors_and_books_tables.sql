c4a76e381dab1d0bb8900b3642a9f340f0da76fb
Mateusz Biel <matbiel@gmail.com>
2023-09-03 19:15:31 +0200

    Configure JOOQ auto generate

---
+++ src/main/resources/db/migration/V1__Create_authors_and_books_tables.sql
@@ -0,0 +1,26 @@
+CREATE TABLE authors (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    age NUMERIC NOT NULL,
+    created TIMESTAMP,
+    updated TIMESTAMP
+);
+
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    isbn VARCHAR(255) NOT NULL,
+    published DATE NOT NULL,
+    genre VARCHAR(255),
+    created TIMESTAMP,
+    updated TIMESTAMP
+);
+
+CREATE TABLE authors_to_books (
+    id SERIAL PRIMARY KEY,
+    author_id INTEGER REFERENCES authors(id),
+    book_id INTEGER REFERENCES books(id),
+    created TIMESTAMP,
+    updated TIMESTAMP
+);
