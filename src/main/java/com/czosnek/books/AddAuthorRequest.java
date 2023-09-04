38d56017d8b4beef4292f53639d2d75cd33850fc
Mateusz Biel <matbiel@gmail.com>
2023-09-04 22:38:33 +0200

    Apply spotless

--- src/main/java/com/czosnek/books/AddAuthorRequest.java
+++ src/main/java/com/czosnek/books/AddAuthorRequest.java
@@ -2,7 +2,6 @@ package com.czosnek.books;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-
 import java.util.List;
 
 public record AddAuthorRequest(
--- src/main/java/com/czosnek/books/AuthorsController.java
+++ src/main/java/com/czosnek/books/AuthorsController.java
@@ -1,12 +1,12 @@
 package com.czosnek.books;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequiredArgsConstructor
